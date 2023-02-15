0d929b3d97d1161436b3eebfaaf2c1dbde1a8b0a
Daniel Garcia <114431876+dagavo1@users.noreply.github.com>
2023-02-15 15:46:10 -0500

    Added target folder without two files (snapshot.war and snapshot.war.original)

---
+++ target/classes/static/js/producto.js
@@ -0,0 +1,80 @@
+function loadData(){
+    let request = sendRequest('/productos/list', 'GET', '');
+    let productos = document.getElementById("section-products");
+    console.log(request);
+    productos.innerHTML = "";
+    request.onload = function(){
+        let data = request.response;
+        data.forEach((element, index) => {
+            
+            productos.innerHTML += `
+            
+            <li class="productos__seccion-item">
+                <a href="vista-producto.html"><img class="producto__imagen" src="${element.refImagen}"></a>
+                <p class="producto__descripcion">${element.infoProducto}</p>
+                <p class="producto__precio">${element.precioProducto}</p>
+            </li>
+        `
+        });
+    }
+    request.onerror = function(){
+        productos.innerHTML = `
+        <h1>ERROR</h1>
+        `
+    }
+}
+
+/* function loadCliente(idcliente){
+    let request = sendRequest('cliente/list/'+idcliente, 'GET', '');
+    let nodocumentoidentidad = document.getElementById('client_noDocumento');
+    let nombre= document.getElementById('client_nombre');
+    let apellido= document.getElementById('client_apellido');
+    let telefono= document.getElementById('client_telefono');
+    let correo= document.getElementById('client_correo');
+    let id = document.getElementById('client_id');
+    request.onload = function(){
+        let data = request.response;
+        id.value = data.idCliente;
+        nodocumentoidentidad.value = data.noDocumentoIdentidad;
+        nombre.value = data.nombre;
+        apellido.value = data.apellido;
+        telefono.value = data.telefono;
+        correo.value = data.correo;
+    }
+    request.onerror = function(){
+        alert('Error al recuperar los datos.')
+    }
+}
+
+function deleteCliente(idcliente){
+    let request = sendRequest('cliente/'+idcliente, 'DELETE', '');
+    request.onload = function(){
+        loadDatas();
+    }
+}
+
+function saveCliente(){
+    let id = document.getElementById('client_id').value;
+    let nodocumentoidentidad = document.getElementById('client_noDocumento').value;
+    let nombre= document.getElementById('client_nombre').value;
+    let apellido= document.getElementById('client_apellido').value;
+    let telefono= document.getElementById('client_telefono').value;
+    let correo= document.getElementById('client_correo').value;
+    let data = {
+        'idCliente': id, 
+        'noDocumentoIdentidad': nodocumentoidentidad, 
+        'nombre': nombre, 
+        'apellido': apellido, 
+        'telefono': telefono, 
+        'correo': correo
+        } 
+    let request = sendRequest('cliente/', id ? 'PUT' : 'POST', data);
+    request.onload = function(){
+        window.location = 'cliente.html';
+    }
+    request.onerror=function(){
+        alert('Error al guardar');
+    }
+}
+ * 
+ */
